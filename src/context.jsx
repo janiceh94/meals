63e3505b7cecee72a8b526678cfffa97368b5709
janiceh94 <haesung94@gmail.com>
2022-11-21 14:19:27 -0800

    yarn add axios

--- src/context.jsx
+++ src/context.jsx
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect } from "react";
 // Context provides a way to pass data through the component tree without having to pass props down manually at every level
+import axios from "axios";
 
 const allMealsURl = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
 const randomMealsUrl = "https://www.themealdb.com/api/json/v1/1/random.php";
