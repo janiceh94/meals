186a459b007cfa0b8f12afc732201215edc732e2
janiceh94 <haesung94@gmail.com>
2022-11-21 13:23:49 -0800

    refactored using useGlobalContext

--- src/components/Meals/Meals.jsx
+++ src/components/Meals/Meals.jsx
@@ -1,7 +1,15 @@
 import "./Meals.css";
+import { useGlobalContext } from "../../context";
 
 export default function Meals(){
+
+    const context = useGlobalContext()
+    console.log(context);
+    //value from context.jsx -> AppContext.Provider
+
     return (
-        <div>Meals</div>
+        <div>
+            Meals
+        </div>
     )
 }
--- src/context.jsx
+++ src/context.jsx
@@ -1,13 +1,21 @@
 import React, { useContext } from "react";
+// Context provides a way to pass data through the component tree without having to pass props down manually at every level
 
 const AppContext = React.createContext();
 
 const AppProvider = ({children}) => {
+    // A provider will sit up as high as possible in the component tree so that multiple consumers can get the state and props from the provider
     return (
-        <AppContext.Provider value="hello">
+        <AppContext.Provider value={{name:"john"}}>
             {children}
+            {/* children is our app */}
         </AppContext.Provider>
     )
 }
 
+export const useGlobalContext = () => {
+    return useContext(AppContext)
+}
+
 export {AppContext, AppProvider}
+
