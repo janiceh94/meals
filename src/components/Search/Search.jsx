02ea2faff91b21c69b0857147827206dbb4ab633
janiceh94 <haesung94@gmail.com>
2022-11-22 10:03:49 -0800

    added search bar and buttons

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,7 +1,19 @@
 import "./Search.css";
+import { useState } from "react";
+import { useGlobalContext} from "../../context";
 
 export default function Search(){
     return (
-        <div>Search</div>
+        <header className="search-container">
+            <form>
+                <input 
+                    type="text" 
+                    placeholder="type favorite meal" 
+                    className="form-input"
+                />
+                <button type="submit" className="btn">Search</button>
+                <button type="button" className="btn btn-hipster">Surprise Me!</button>
+            </form>
+        </header>
     )
 }
